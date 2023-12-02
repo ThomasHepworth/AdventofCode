c9d2acc9d517e0ae5da1b1a340f28da1230a098a
Tom Hepworth <thomas.hepworth@justice.gov.uk>
2023-12-02 17:07:34 +0000

    use list comprehension for loops

--- 2023/python/day-02/day-02.py
+++ 2023/python/day-02/day-02.py
@@ -1,5 +1,6 @@
 from collections import Counter
 import math
+from functools import reduce
 
 MAX_COUNTERS = {"red": 12, "green": 13, "blue": 14}
 valid_games = 0  # part 1
@@ -8,17 +9,13 @@ product_sums = 0  # part 2
 # with open ('dummy_input.txt') as f:
 with open ('input.txt') as f:
     for game_id, game in enumerate(f.readlines(), 1):  # games are sequential, so we can use enumerate
-        # A counter object to track the largest number of each colour encountered
-        game_counter = Counter()
         # Removes 'Game {}:' and pulls out the subgames
         subgames = [game_moves.split(", ") for game_moves in game.strip().split(": ")[1].split("; ")]
-
-        # Clean up our subgames so that each move is in the form -> ("3",  "blue")
-        for subgame in subgames:
-            moves = (move.split(" ") for move in subgame)
-            for number, colour in moves:
-                game_counter = game_counter | Counter({colour: int(number)})
-
+        subgames = [(move.split(" ") for move in subgame) for subgame in subgames]
+        # Produce a series of Counters
+        subgame_counters = [Counter({colour: int(num) for num, colour in subgame}) for subgame in subgames]
+        # Counter | Counter -> yields the largest
+        game_counter = reduce(lambda x, y: x|y, subgame_counters)
         # Part 1
         if all(MAX_COUNTERS[colour] >= game_counter[colour] for colour in MAX_COUNTERS):
             valid_games += game_id
