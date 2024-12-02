8092dbe9b6e97d40d885130a229ac6ec8651783f
Tom Hepworth <thomas.hepworth@justice.gov.uk>
2024-12-02 17:40:55 +0000

    fix: update python AoC day creation script

--- create_day/advent_of_code_day_py.sh
+++ create_day/advent_of_code_day_py.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Process command-line arguments
+YEAR=""
+DAY=""
+
 while getopts y:d: flag
 do
     case "${flag}" in
@@ -9,22 +11,29 @@ do
     esac
 done
 
-# Format day with leading zero
-FORMATTED_DAY=$(printf "%02d" $DAY)
+if [ -z "$YEAR" ]; then
+    echo "Error: Year (-y) is required."
+    exit 1
+fi
 
-# Create directory structure
+if [ -z "$DAY" ]; then
+    echo "Error: Day (-d) is required."
+    exit 1
+fi
+
+FORMATTED_DAY=$(printf "%02d" $DAY)
 DIR_PATH="${YEAR}/python/day-${FORMATTED_DAY}"
 mkdir -p $DIR_PATH
 
-# Copy Python template file if it exists
 if [ -f create_day/template_code/sol.py ]; then
     cp create_day/template_code/sol.py $DIR_PATH/
 else
     echo "Python template file not found. Creating an empty sol.py file."
     touch $DIR_PATH/sol.py
 fi
+
+# Create input files
 touch $DIR_PATH/input.txt
 touch $DIR_PATH/dummy_input.txt
 
-
-echo "AoC ${YEAR} day ${DAY} created for python."
+echo "AoC ${YEAR} day ${FORMATTED_DAY} created for python."
